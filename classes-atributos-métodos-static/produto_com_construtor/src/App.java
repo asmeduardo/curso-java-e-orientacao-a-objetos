9bb4ccc5a0fe8c480e9497a05496beee068f8d76
Eduardo Melo <eduardo.melo01992@gmail.com>
2025-06-13 14:44:07 -0300

    Cria um produto com o auxílio de um construtor

---
+++ classes-atributos-métodos-static/produto_com_construtor/src/App.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product p = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + p);
+
+        System.out.println();
+        System.out.print("Enter the number of the products to be added in stock: ");
+        p.addProducts(sc.nextInt());
+
+        System.out.println();
+        System.out.println("Update data: " + p);
+
+        System.out.println();
+        System.out.print("Enter the number of the products to be removed from stock: ");
+        p.removeProducts(sc.nextInt());
+
+        System.out.println();
+        System.out.println("Update data: " + p);
+
+        sc.close();
+    }
+}
